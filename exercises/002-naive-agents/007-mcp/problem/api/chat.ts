69ab41e8c457fb67dae80a340a28de00b1fafea7
Matt Pocock <mattpocockvoice@gmail.com>
2025-07-08 13:06:56 +0100

    Added MCP

---
+++ exercises/002-naive-agents/007-mcp/problem/api/chat.ts
@@ -0,0 +1,27 @@
+import { google } from "@ai-sdk/google";
+import {
+  convertToModelMessages,
+  stepCountIs,
+  streamText,
+  type UIMessage,
+} from "ai";
+
+if (!process.env.GITHUB_PERSONAL_ACCESS_TOKEN) {
+  throw new Error("GITHUB_PERSONAL_ACCESS_TOKEN is not set");
+}
+
+export const POST = async (req: Request): Promise<Response> => {
+  const body: { messages: UIMessage[] } = await req.json();
+  const { messages } = body;
+
+  const result = streamText({
+    model: google("gemini-2.5-flash"),
+    messages: convertToModelMessages(messages),
+    system: `
+      You are a helpful assistant that can use the GitHub API to interact with the user's GitHub account.
+    `,
+    stopWhen: [stepCountIs(10)],
+  });
+
+  return result.toUIMessageStreamResponse();
+};
--- exercises/002-naive-agents/007-mcp/solution/api/chat.ts
+++ exercises/002-naive-agents/007-mcp/solution/api/chat.ts
@@ -3,48 +3,50 @@ import {
   convertToModelMessages,
   stepCountIs,
   streamText,
-  tool,
   type UIMessage,
 } from "ai";
-import { z } from "zod";
 
 import { experimental_createMCPClient as createMCPClient } from "ai";
 import { Experimental_StdioMCPTransport as StdioMCPTransport } from "ai/mcp-stdio";
 
-const mcpClient = await createMCPClient({
-  transport: new StdioMCPTransport({
-    command: "node",
-    args: ["src/stdio/dist/server.js"],
-    env: {},
-  }),
-});
+if (!process.env.GITHUB_PERSONAL_ACCESS_TOKEN) {
+  throw new Error("GITHUB_PERSONAL_ACCESS_TOKEN is not set");
+}
 
 export const POST = async (req: Request): Promise<Response> => {
   const body: { messages: UIMessage[] } = await req.json();
   const { messages } = body;
 
+  const mcpClient = await createMCPClient({
+    transport: new StdioMCPTransport({
+      command: "docker",
+      args: [
+        "run",
+        "-i",
+        "--rm",
+        "-e",
+        "GITHUB_PERSONAL_ACCESS_TOKEN",
+        "ghcr.io/github/github-mcp-server",
+      ],
+      env: {
+        GITHUB_PERSONAL_ACCESS_TOKEN: process.env.GITHUB_PERSONAL_ACCESS_TOKEN!,
+      },
+    }),
+  });
+
   const result = streamText({
     model: google("gemini-2.5-flash"),
     messages: convertToModelMessages(messages),
     system: `
-      You are a helpful assistant that can use a sandboxed file system to create, edit and delete files.
-
-      You have access to the following tools:
-      - writeFile
-      - readFile
-      - deletePath
-      - listDirectory
-      - createDirectory
-      - exists
-      - searchFiles
-
-      Use these tools to record notes, create todo lists, and edit documents for the user.
-
-      Use markdown files to store information.
+      You are a helpful assistant that can use the GitHub API to interact with the user's GitHub account.
     `,
-    tools: {},
+    tools: await mcpClient.tools({ schemas: "automatic" }),
     stopWhen: [stepCountIs(10)],
   });
 
-  return result.toUIMessageStreamResponse();
+  return result.toUIMessageStreamResponse({
+    onFinish: async () => {
+      await mcpClient.close();
+    },
+  });
 };
