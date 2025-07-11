08e464437e0d3a2b213a910443259c93a5589ba2
Matt Pocock <mattpocockvoice@gmail.com>
2025-07-11 12:03:16 +0100

    Updated format of solution

--- exercises/007-rag/018-retrieval-with-bm25/solution/api/chat.ts
+++ exercises/007-rag/018-retrieval-with-bm25/solution/api/chat.ts
@@ -6,10 +6,8 @@ import {
   streamText,
   type UIMessage,
 } from "ai";
-import { readdir, readFile } from "fs/promises";
-import BM25 from "okapibm25";
-import path from "path";
 import z from "zod";
+import { searchTypeScriptDocs } from "./bm25.ts";
 
 export type MyMessage = UIMessage<
   unknown,
@@ -32,44 +30,6 @@ const formatMessageHistory = (messages: UIMessage[]) => {
         .join("")}`;
     })
     .join("\n");
-};
-
-const loadTsDocs = async () => {
-  const TS_DOCS_LOCATION = path.resolve(
-    import.meta.dirname,
-    "../../../../../datasets/ts-docs"
-  );
-
-  const files = await readdir(TS_DOCS_LOCATION);
-
-  const docs = await Promise.all(
-    files.map(async (file) => {
-      const filePath = path.join(TS_DOCS_LOCATION, file);
-      const content = await readFile(filePath, "utf8");
-      return {
-        filename: file,
-        content,
-      };
-    })
-  );
-
-  return docs;
-};
-
-const searchTypeScriptDocs = async (keywords: string[]) => {
-  const docs = await loadTsDocs();
-
-  const scores: number[] = (BM25 as any)(
-    docs.map((doc) => doc.content),
-    keywords
-  );
-
-  return scores
-    .map((score, index) => ({
-      score,
-      doc: docs[index],
-    }))
-    .sort((a, b) => b.score - a.score);
 };
 
 export const POST = async (req: Request): Promise<Response> => {
