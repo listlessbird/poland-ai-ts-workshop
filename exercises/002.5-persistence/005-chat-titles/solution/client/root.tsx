b11e4434baae0824e7ebc91b103f0fd91b037188
Matt Pocock <mattpocockvoice@gmail.com>
2025-07-24 09:12:23 +0100

    Test

---
+++ exercises/002.5-persistence/005-chat-titles/solution/client/root.tsx
@@ -0,0 +1,75 @@
+import { Chat, useChat } from '@ai-sdk/react';
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChatInput, Message, Wrapper } from './components.tsx';
+import './tailwind.css';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+  useSuspenseQuery,
+} from '@tanstack/react-query';
+import type { DB } from '../api/persistence-layer.ts';
+
+const App = () => {
+  const [backupId] = useState(() => crypto.randomUUID());
+  const searchParams = new URLSearchParams(
+    window.location.search,
+  );
+  const chatIdFromSearchParams = searchParams.get('chatId');
+
+  const { data } = useSuspenseQuery({
+    queryKey: ['chat', chatIdFromSearchParams],
+    queryFn: () => {
+      if (!chatIdFromSearchParams) {
+        return null;
+      }
+
+      return fetch(
+        `/api/chat?chatId=${chatIdFromSearchParams}`,
+      ).then((res): Promise<DB.Chat> => res.json());
+    },
+  });
+
+  const chat = new Chat({
+    id: chatIdFromSearchParams ?? backupId,
+    messages: data?.messages ?? [],
+  });
+
+  const { messages, sendMessage } = useChat({
+    chat,
+  });
+
+  const [input, setInput] = useState('');
+
+  return (
+    <Wrapper>
+      {messages.map((message) => (
+        <Message
+          key={message.id}
+          role={message.role}
+          parts={message.parts}
+        />
+      ))}
+      <ChatInput
+        input={input}
+        onChange={(e) => setInput(e.target.value)}
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage({
+            text: input,
+          });
+          setInput('');
+          window.history.pushState({}, '', `?chatId=${chat.id}`);
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <QueryClientProvider client={new QueryClient()}>
+    <App />
+  </QueryClientProvider>,
+);
