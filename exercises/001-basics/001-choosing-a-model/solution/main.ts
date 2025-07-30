f172dd90c6f5babc09ec96fadaa3758375e4d68c
Matt Pocock <mattpocockvoice@gmail.com>
2025-07-30 11:19:27 +0100

    Adjusted 001

---
+++ exercises/001-basics/001-choosing-a-model/solution/main.ts
@@ -0,0 +1,15 @@
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+
+// TODO: Choose a model. I recommend using the Google Gemini model:
+// gemini-2.0-flash
+const model = google('gemini-2.0-flash');
+
+const prompt = 'What is the capital of France?';
+
+const result = await generateText({
+  model,
+  prompt,
+});
+
+console.log(result.text);
