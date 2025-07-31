120a46614b73cff4208a214297c46babf2181429
Matt Pocock <mattpocockvoice@gmail.com>
2025-07-31 09:14:13 +0100

    onFinish

--- exercises/003-persistence/008-on-finish/explainer/api/chat.ts
+++ exercises/003-persistence/008-on-finish/explainer/api/chat.ts
@@ -1,10 +1,9 @@
+import { google } from '@ai-sdk/google';
 import {
   convertToModelMessages,
-  createUIMessageStream,
   streamText,
   type UIMessage,
 } from 'ai';
-import { google } from '@ai-sdk/google';
 
 export const POST = async (req: Request): Promise<Response> => {
   const body: { messages: UIMessage[] } = await req.json();
@@ -14,6 +13,9 @@ export const POST = async (req: Request): Promise<Response> => {
     model: google('gemini-2.0-flash'),
     messages: convertToModelMessages(messages),
     onFinish: ({ response }) => {
+      // 'response.messages' is an array of ToolModelMessage and AssistantModelMessage,
+      // which are the model messages that were generated during the stream.
+      // This is useful if you don't need UIMessages - for simpler applications.
       console.log('streamText.onFinish');
       console.log('  response.messages');
       console.dir(response.messages, { depth: null });
@@ -21,10 +23,15 @@ export const POST = async (req: Request): Promise<Response> => {
   });
 
   return result.toUIMessageStreamResponse({
+    originalMessages: messages,
     onFinish: ({ messages, responseMessage }) => {
+      // 'messages' is the full message history, including the original messages
+      // you pass in to originalMessages.
       console.log('toUIMessageStreamResponse.onFinish');
       console.log('  messages');
       console.dir(messages, { depth: null });
+
+      // 'responseMessage' is the last message in the message history.
       console.log('toUIMessageStreamResponse.onFinish');
       console.log('  responseMessage');
       console.dir(responseMessage, { depth: null });
